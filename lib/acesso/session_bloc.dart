a82a5586113201b0e03402daab5c72fe92973a3a
Gabriel Silva <gabriel@mainclass.com.br>
2023-07-21 11:23:26 -0300

    Permitindo atualizar dados do usuario na sessao

--- lib/acesso/session_bloc.dart
+++ lib/acesso/session_bloc.dart
@@ -1,5 +1,7 @@
 part of main_class.acesso;
 
+typedef AlteradorUsuario = Function(UsuarioLogado usuario);
+
 class SessionBloc implements Bloc {
   late BehaviorSubject<UsuarioLogado?> _usuario;
 
@@ -17,11 +19,8 @@ class SessionBloc implements Bloc {
     acessoHandler.dispose();
   }
 
-  Future<UsuarioLogado> login(
-    String username,
-    String password,
-    [Map<String, dynamic>? extras]
-  ) async {
+  Future<UsuarioLogado> login(String username, String password,
+      [Map<String, dynamic>? extras]) async {
     UsuarioLogado usuario =
         await acessoHandler.login(username, password, extras);
 
@@ -32,6 +31,14 @@ class SessionBloc implements Bloc {
 
   _add(UsuarioLogado? usuario) async {
     _usuario.add(usuario);
+  }
+
+  update(AlteradorUsuario action) {
+    if (currentUsuario != null) {
+      action(currentUsuario!);
+    }
+
+    _add(currentUsuario);
   }
 
   logout() async {
